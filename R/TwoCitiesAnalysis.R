785cd1925fe14a3980acfc2204db2a2483c064ba
Kezia Manlove <kezia.manlove@gmail.com>
2015-04-14 08:34:44 -0600

    Add journal communities into IncludedPaperBank_Final in TwoCitiesAnalysis

--- R/TwoCitiesAnalysis.R
+++ R/TwoCitiesAnalysis.R
@@ -15,6 +15,12 @@ source("./R/TwoCities_SourceFunctions.R")
 #-------------------------------------------#
 data.frame <- read.csv("./Data/IncludedPaperBank_Final.csv", header = T, sep = "\t")
 data.frame <- DataPrep(data.frame)
+jo.comms <- read.csv("./Data/JournalCommunityVector_16Dec2014.csv")
+jo.comms <- jo.comms[-c(1, 494, 603, 858), 2] # four papers removed in DataPrep function
+data.frame$Journal.Community <- jo.comms
+comm1 <- subset(data.frame, Journal.Community == 1)
+comm2 <- subset(data.frame, Journal.Community == 2)
+comm3 <- subset(data.frame, Journal.Community == 3)
 
 JournalFreqHist(data.frame)
 TimesCitedHists(data.frame)
@@ -38,28 +44,29 @@ paper.diags <- NetworkDiagnostics(graph.in = paper.graph, seed.in = 123)
 journal.data <- ExtractJournalData(data.frame)
 journal.graph <- BuildJournalGraph(data.frame.in = data.frame,
                                    assoc.mat.in = assoc.mat)
+# 26 journals get cut when constructing journal.graph....
 
 #-----------------------------------------#
 #-- Author network -----------------------#
 #-----------------------------------------#
-par(mfrow = c(1, 1))
-plot(author.graph, margin = c(-.75, -.75, 0, 0), 
-     vertex.size = 1, 
-     vertex.label = NA, 
-     edge.arrow.size = .05)
+# par(mfrow = c(1, 1))
+# plot(author.graph, margin = c(-.75, -.75, 0, 0), 
+#      vertex.size = 1, 
+#      vertex.label = NA, 
+#      edge.arrow.size = .05)
 
 # cut down to just giant component
 author.clusters <- clusters(author.graph)
 giant.compo.aus <- delete.vertices(author.graph, which(clusters(author.graph)$membership != 1))
 
-plot(giant.compo.aus, 
-     margin = c(-.85, -.75, -.15, -.5), 
-     vertex.size = 1, 
-     vertex.label = NA, 
-     edge.arrow.size = .05)
+# plot(giant.compo.aus, 
+#      margin = c(-.85, -.75, -.15, -.5), 
+#      vertex.size = 1, 
+#      vertex.label = NA, 
+#      edge.arrow.size = .05)
 
-stored.layout <- layout.fruchterman.reingold(giant.compo.aus)
-margin.dims <- c(-1.0, -1.0, -.15, -.9)
+# stored.layout <- layout.fruchterman.reingold(giant.compo.aus)
+# margin.dims <- c(-1.0, -1.0, -.15, -.9)
 giant.compo.au.walktr <- walktrap.community(giant.compo.aus, steps = 4)
 giant.compo.au.walktr$membership
 table(giant.compo.au.walktr$membership)
@@ -77,41 +84,41 @@ for(i in 1:length(levels(au.vertex.ind))){
   au.comm.top10[[i]] <- au.comm.i$name[order(au.comm.i$size, decreasing = T)][1:10]
 }
 
-plot(giant.compo.aus, 
-     vertex.frame.color = as.numeric(walktr.au.giant$membership), 
-     vertex.label = vertex.labels.giantcompos,  
-     vertex.label.cex = .5, 
-     vertex.label.color = "black", 
-     margin = c(-.9, -.95, -.3, -.7), 
-     layout = layout.fruchterman.reingold, 
-     edge.width = 0.25, 
-     edge.color = "grey50", 
-     edge.arrow.size = 0, 
-     vertex.label = "", 
-     vertex.size = V(giant.compo.aus)$size / 12, 
-     vertex.color = as.numeric(walktr.au.giant$membership))
-
-stored.layout.au <- layout.fruchterman.reingold(giant.compo.aus)
-
-plot(giant.compo.aus, 
-     margin = margin.dims, 
-     vertex.frame.color = "black", 
-     vertex.label = "",  
-     vertex.label.cex = .6, 
-     vertex.label.color = "black", 
-     layout = stored.layout.au, 
-     edge.width = 0.25, 
-     edge.color = "grey50", 
-     edge.arrow.size = 0, 
-     vertex.label = "", 
-     vertex.size = V(giant.compo.aus)$size / 12, 
-     vertex.color = au.vertex.colors)
+# plot(giant.compo.aus, 
+#      vertex.frame.color = as.numeric(walktr.au.giant$membership), 
+#      vertex.label = vertex.labels.giantcompos,  
+#      vertex.label.cex = .5, 
+#      vertex.label.color = "black", 
+#      margin = c(-.9, -.95, -.3, -.7), 
+#      layout = layout.fruchterman.reingold, 
+#      edge.width = 0.25, 
+#      edge.color = "grey50", 
+#      edge.arrow.size = 0, 
+#      vertex.label = "", 
+#      vertex.size = V(giant.compo.aus)$size / 12, 
+#      vertex.color = as.numeric(walktr.au.giant$membership))
+#
+# stored.layout.au <- layout.fruchterman.reingold(giant.compo.aus)
+#
+# plot(giant.compo.aus, 
+#      margin = margin.dims, 
+#      vertex.frame.color = "black", 
+#      vertex.label = "",  
+#      vertex.label.cex = .6, 
+#      vertex.label.color = "black", 
+#      layout = stored.layout.au, 
+#      edge.width = 0.25, 
+#      edge.color = "grey50", 
+#      edge.arrow.size = 0, 
+#      vertex.label = "", 
+#      vertex.size = V(giant.compo.aus)$size / 12, 
+#      vertex.color = au.vertex.colors)
 
 au.dendro <- as.dendrogram(giant.compo.au.walktr)
 height.of.leafs <- dendrapply(au.dendro, function(e) attr(e, "height"))
 quantile(unlist(height.of.leafs), c(0.9, 0.95, 0.975))
 au.dendro.height2700 <- cut(au.dendro, h = 2700)
-plot(au.dendro.height2700$upper)
+# plot(au.dendro.height2700$upper)
 
 quantile(V(giant.compo.aus)$size, 0.95)
 V(giant.compo.aus)$name[which(V(giant.compo.aus)$size >= 24)]
@@ -121,8 +128,8 @@ which(V(giant.compo.aus)$size >= 24)[1]
 #-- citation network -------------#
 #---------------------------------# 
 # plots for paper graph
-par(mfrow = c(1, 1))
-plot(paper.graph, vertex.size = 1, vertex.label = NA, edge.arrow.size = .05)
+# par(mfrow = c(1, 1))
+# plot(paper.graph, vertex.size = 1, vertex.label = NA, edge.arrow.size = .05)
 paper.degree.dist <- degree(paper.graph, mode = "in")
 paper.betweenness <- betweenness(paper.graph)
 paper.hierarchy <- hierarchy(paper.graph)
@@ -149,47 +156,47 @@ for(i in 1:length(levels(paper.vertex.ind))){
   paper.comm.top10[[i]] <- paper.comm.i$name[order(paper.comm.i$size, decreasing = T)][1:10]
 }
 
-margin.dims = c(0, 0, 0, 0)
-plot(giant.compo.papers, 
-     margin = margin.dims, 
-     vertex.frame.color = "black", 
-     vertex.label = "",  
-     vertex.label.cex = .5, 
-     vertex.label.color = "black", 
-     layout = fixed.paper.layout, 
-     edge.width = 0.25, 
-     edge.color = "grey50", 
-     edge.arrow.size = 0, 
-     vertex.label = "", 
-     vertex.size = V(giant.compo.papers)$size / 30, 
-     vertex.color = paper.vertex.colors)
+# margin.dims = c(0, 0, 0, 0)
+# plot(giant.compo.papers, 
+#      margin = margin.dims, 
+#      vertex.frame.color = "black", 
+#      vertex.label = "",  
+#      vertex.label.cex = .5, 
+#      vertex.label.color = "black", 
+#      layout = fixed.paper.layout, 
+#      edge.width = 0.25, 
+#      edge.color = "grey50", 
+#      edge.arrow.size = 0, 
+#      vertex.label = "", 
+#      vertex.size = V(giant.compo.papers)$size / 30, 
+#      vertex.color = paper.vertex.colors)
 
 #-----------------------------------------#
 #-- Journal network ----------------------#
 #-----------------------------------------#
 
-order.source <- table(data.frame$Source)[order(table(data.frame$Source), decreasing = T)]
-# order command reorders levels so that they appear in descending frequency
-par(mfrow = c(1, 1), las = 2, mar = c(15, 6, 1, 1))
-plot(journal.agg.annual.cites[order(journal.agg.annual.cites, decreasing = T)] ~ c(1:112), 
-     type = "h", 
-     xaxt = "n", 
-     ylab = "Avg. annualize citation rate for \n all papers in dataset", 
-     xlab = "", 
-     lwd = journal.total.papers[order(journal.agg.annual.cites, decreasing = T)] * .5)
-text(journal.agg.annual.cites[order(journal.agg.annual.cites, decreasing = T)][1:20] ~ c(seq(1:20) + 0.25), 
-     labels = paste("(", journal.total.papers[order(journal.agg.annual.cites, decreasing = T)][1:20], ")", sep = ""), 
-     cex = .6)
-axis(side = 1, las = 2, cex.axis = .5,
-     at = c(1:112), 
-     labels = levels(data.frame$Source)[order(journal.agg.annual.cites, decreasing = T)])
-
-
-plot(journal.graph)
+# order.source <- table(data.frame$Source)[order(table(data.frame$Source), decreasing = T)]
+# # order command reorders levels so that they appear in descending frequency
+# par(mfrow = c(1, 1), las = 2, mar = c(15, 6, 1, 1))
+# plot(journal.agg.annual.cites[order(journal.agg.annual.cites, decreasing = T)] ~ c(1:112), 
+#      type = "h", 
+#      xaxt = "n", 
+#      ylab = "Avg. annualize citation rate for \n all papers in dataset", 
+#      xlab = "", 
+#      lwd = journal.total.papers[order(journal.agg.annual.cites, decreasing = T)] * .5)
+# text(journal.agg.annual.cites[order(journal.agg.annual.cites, decreasing = T)][1:20] ~ c(seq(1:20) + 0.25), 
+#      labels = paste("(", journal.total.papers[order(journal.agg.annual.cites, decreasing = T)][1:20], ")", sep = ""), 
+#      cex = .6)
+# axis(side = 1, las = 2, cex.axis = .5,
+#      at = c(1:112), 
+#      labels = levels(data.frame$Source)[order(journal.agg.annual.cites, decreasing = T)])
+# 
+# 
+# plot(journal.graph)
 # Qu: which journals are excluded?
-disconnected.journals <- levels(factor(data.frame$Source))
-[!(levels(factor(data.frame$Source)) %in% levels(journal.frame$journal1.vec))]
-V(journal.graph)$size <- table(data.frame$Source)[!(names(table(data.frame$Source)) %in% disconnected.journals)]
+# disconnected.journals <- levels(factor(data.frame$Source))
+# [!(levels(factor(data.frame$Source)) %in% levels(journal.frame$journal1.vec))]
+# V(journal.graph)$size <- table(data.frame$Source)[!(names(table(data.frame$Source)) %in% disconnected.journals)]
 
 # # description of journal graph
 # journal.compos <- clusters(journal.graph, mode = "weak")
@@ -201,54 +208,109 @@ V(journal.graph)$size <- table(data.frame$Source)[!(names(table(data.frame$Sourc
 # diam.journal <- diameter(journal.graph)
 # power.law.fit.journal <- power.law.fit(degree(journal.graph, mode = "all"))
 
-journal.graph.small <- delete.vertices(journal.graph, which(degree(journal.graph) < 1) - 1)
+# which(degree(journal.graph) < 1)
+comm1.jos <- levels(factor(subset(data.frame, Journal.Community == 1)$Source))
+comm2.jos <- levels(factor(subset(data.frame, Journal.Community == 2)$Source))
+comm3.jos <- levels(factor(subset(data.frame, Journal.Community == 3)$Source))
+
+journal.graph.small <- delete.vertices(journal.graph, which(degree(journal.graph) < 1))
+vertex.sizes <- vertex.community <- rep(NA, length(V(journal.graph.small)))
+for(i in 1:length(vertex.sizes)){
+  k <- subset(journal.data, journal == V(journal.graph.small)$name[i])
+  vertex.sizes[i] <- as.numeric(as.character(k$total.papers))[1]
+  vertex.community[i] <- ifelse(V(journal.graph.small)$name[i] %in% comm1.jos, 1,
+                                ifelse(V(journal.graph.small)$name[i] %in% comm2.jos, 2,
+                                       ifelse(V(journal.graph.small)$name[i] %in% comm3.jos, 3, 4)))
+}
+
 V(journal.graph.small)$name
 plot(journal.graph.small, 
      layout = layout.fruchterman.reingold, 
-     vertex.size = log(table(journal.frame$journal1.vec) + 1) * 2, 
+#     vertex.size = log(table(journal.frame$journal1.vec) + 1) * 2, 
+     vertex.size = log(vertex.sizes + 1) * 2, 
      vertex.label = NA, 
      edge.arrow.size = .05)
 
-# journal communities
-set.seed(123)
-walktr.jo <- walktrap.community(journal.graph.small, steps = 4)
-walktr.jo$membership
-table(walktr.jo$membership)
-# largest communities get unique colors; all communities <= 10 are "grey40"
-small.communities.jo <- as.numeric(as.character(names(table(walktr.jo$membership))
-                                                [which(table(walktr.jo$membership) <= 3)]))
+# # journal communities
+# set.seed(123)
+# walktr.jo <- walktrap.community(journal.graph.small, steps = 4)
+# walktr.jo$membership
+# V(journal.graph.small)$name[walktr.jo$membership == 3]
+# table(walktr.jo$membership)
+# # largest communities get unique colors; all communities <= 10 are "grey40"
+# small.communities.jo <- as.numeric(as.character(names(table(walktr.jo$membership))
+#                                                 [which(table(walktr.jo$membership) <= 3)]))
+# jo.vertex.ind <- factor(ifelse(walktr.jo$membership %in% small.communities.jo, 
+#                                "small", 
+#                                walktr.jo$membership))
 
 
-jo.vertex.ind <- factor(ifelse(walktr.jo$membership %in% small.communities.jo, 
-                               "small", 
-                               walktr.jo$membership))
-color.vec.jo <- c("deeppink", "blue", "gold1", "grey70")
-jo.vertex.colors <- color.vec.jo[jo.vertex.ind]
+
+vet.vertices <- c(5, 52, 79, 89, 92, 94, 95)
+ecol.vertices <- c(72, 26, 50, 6, 29, 28, 48, 37, 23, 70)  
+genbiol.vertices <- c(77, 59, 65, 60, 81, 91, 80, 76, 57, 35)
+
+jo.vertex.ind <- rep(4, length(V(journal.graph.small)))
+jo.vertex.ind[vet.vertices] <- 3
+jo.vertex.ind[ecol.vertices] <- 2
+jo.vertex.ind[genbiol.vertices] <- 1
+
+# jo.vertex.ind <- journal
+color.vec.jo <- c("pink", "lightblue", "gold1", "grey70")
+jo.vertex.colors <- color.vec.jo[vertex.community]
 V(journal.graph.small)$size <- ifelse(is.finite(V(journal.graph.small)$size) == T, 
                                       V(journal.graph.small)$size, 0)
 
-jo.comm.top10 <- vector("list", length(levels(jo.vertex.ind)))
-for(i in 1:length(levels(jo.vertex.ind))){
-  jo.comm.i <- V(journal.graph.small)[as.numeric(jo.vertex.ind) == i]
-  jo.comm.top10[[i]] <- jo.comm.i$name[order(jo.comm.i$size, decreasing = T)][1:10]
-}
+# jo.comm.top10 <- vector("list", length(levels(jo.vertex.ind)))
+# for(i in 1:length(levels(jo.vertex.ind))){
+#   jo.comm.i <- V(journal.graph.small)[as.numeric(jo.vertex.ind) == i]
+#   jo.comm.top10[[i]] <- jo.comm.i$name[order(jo.comm.i$size, decreasing = T)][1:10]
+# }
 
 fixed.jo.layout <- layout.fruchterman.reingold(journal.graph.small)
-margin.dims = c(0, 0, 0, 0)
+par(mfrow = c(1, 1))
+margin.dims = c(-1.0, -.75, 0, -.25)
+
+# cherry-pick journal names to label
+vertex.labels <- rep("", 96)
+vertex.labels[which(vertex.sizes >= 30)] <- V(journal.graph.small)$name[which(vertex.sizes >= 30)]
+vertex.labels[6] <- "Am Nat"
+vertex.labels[27] <- "Ecol Monogs"
+vertex.labels[28] <- "Ecology"
+vertex.labels[51] <- "J Clin Micro"
+vertex.labels[48] <- "J Anim Ecol"
+vertex.labels[58] <- "J Med Ent"
+vertex.labels[60] <- "Interface"
+vertex.labels[61] <- "J Wildlife Dis"
+vertex.labels[62] <- "J Wildlife Manag"
+vertex.labels[66] <- "Med Vet Ent"
+vertex.labels[73] <- "PLoS Biol"
+vertex.labels[75] <- "PLoS Med"
+vertex.labels[77] <- "PLoS One"
+vertex.labels[78] <- "Pop Health Metrics"
+vertex.labels[79] <- "Prev Vet Med"
+vertex.labels[80] <- "PNAS"
+vertex.labels[81] <- "PRSB"
+vertex.labels[82] <- "Science"
+vertex.labels[85] <- "Stats in Med"
+vertex.labels[91] <- "Vaccine"
+vertex.labels[96] <- "Vet Parasitol"
+
 
 plot(journal.graph.small, 
      margin = margin.dims, 
      vertex.frame.color = "black", 
      vertex.label = "",  
-     vertex.label.cex = .5, 
+     vertex.label.cex = .7, 
      vertex.label.color = "black", 
      layout = fixed.jo.layout, 
      edge.width = 0.25, 
      edge.color = "grey50", 
      edge.arrow.size = 0, 
-     vertex.label = "", 
-     vertex.size = (V(journal.graph.small)$size + 1) / 10 + 2, 
+#     vertex.label = V(journal.graph.small)$name, 
+     vertex.size = log(vertex.sizes + 1) * 3, 
      vertex.color = jo.vertex.colors)
+legend(x = -1, y = -.6, c("Ecology", "General Bio", "Vet", "Other"), bty = "n", fill = c("pink", "lightblue", "gold1", "grey70"))
 
 jo.dendro <- as.dendrogram(walktr.jo)
 in.colbar <- c("forestgreen", 
@@ -362,9 +424,31 @@ full.reader.assignments <- as.data.frame(rbind(comm1.reader.frame,
 #    full frame, extracting authorID, and subsetting on authorID in author.unique.frame
 # 2) sum (math, stat, ecol, evol, epi, med) across all paper authors
 data.frame.full <- DataFrameAddons(data.frame.in = data.frame, all.authors = affils.test, unique.authors)
-
+comm1.full <- subset(data.frame.full, Journal.Community == 1)
+comm2.full <- subset(data.frame.full, Journal.Community == 2)
+comm3.full <- subset(data.frame.full, Journal.Community == 3)
 # PLOTS: author discipline diversity and Number center affiliations by annualized citation rate
 AuthorFactorsByCitesPlot(data.frame = data.frame.full)
+AuthorFactorsByCitesPlot(data.frame = comm1.full)
+AuthorFactorsByCitesPlot(data.frame = comm2.full)
+AuthorFactorsByCitesPlot(data.frame = comm3.full)
+
+AuthorDiversByCitesComparPlot(comm1.dat = comm1.full, comm2.dat = comm2.full,
+                              comm3.dat = comm3.full)
+
+# does author centrality for all previous papers predict author centrality on this paper?
+
+
+# plot(comm1.full$author.diversity ~ as.factor(comm1.full$PubYear), pch = 16, col = "lightblue")
+# plot(comm2.full$author.diversity ~ as.factor(comm2.full$PubYear), pch = 16, col = "pink")
+# plot(comm3.full$author.diversity ~ as.factor(comm3.full$PubYear), pch = 16, col = "gold")
+
+# author centrality through time figure
+par(mfrow = c(1, 1))
+plot(data.frame.full$author.diversity ~ as.factor(data.frame.full$PubYear), 
+     pch = 16, col = "grey60", xlab = "", ylab = "author diversity", xaxt = "n")
+axis(side = 1, at = c(2, 7, 12, 17, 22), labels = c("1990", "1995", "2000", "2005", "2010"))
+
 
 papers.with.citrate <- subset(data.frame, is.na(AnnualizedCitationRate) == F & 
                                 is.infinite(AnnualizedCitationRate) == F)
