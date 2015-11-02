caeb437a251d991d60c2b917c90c4f67fd53d264
Kezia Manlove <kezia.manlove@gmail.com>
2015-11-02 10:55:25 -0700

    Build AuthorDiversityPlot function which builds fig S5

--- R/AuthorDiversityPlots.R
+++ R/AuthorDiversityPlots.R
@@ -7,27 +7,33 @@ AuthorDiversityPlots <- function(data.frame){
   comm1.byyear <- comm2.byyear <- comm3.byyear <- vector("list", 35)
   pi.1 <- pi.2 <- pi.3 <- vector("list", 35)
   shannon.comm1 <- shannon.comm2 <- shannon.comm3 <- rep(NA, 35)
+  papers.comm1 <- papers.comm2 <- papers.comm3 <- rep(NA, 35)
   
   for(i in 1:35){
     comm1.byyear[[i]] <- subset(data.frame, Journal.Community == 1 
                                 & PubYear == 1979 + i)
     pi.1[[i]] <- table(comm1.byyear[[i]]$AuthInstAffil) / dim(comm1.byyear[[i]])
     shannon.comm1[i] <- diversity(pi.1[[i]])
+    papers.comm1[i] <- dim(comm1.byyear[[i]])[1]
     
     comm2.byyear[[i]] <- subset(data.frame, Journal.Community == 2 
                                 & PubYear == 1979 + i)
     pi.2[[i]] <- table(comm2.byyear[[i]]$AuthInstAffil) / dim(comm2.byyear[[i]])
     shannon.comm2[i] <- diversity(pi.2[[i]])
-    
+    papers.comm2[i] <- dim(comm2.byyear[[i]])[1]
+
     comm3.byyear[[i]] <- subset(data.frame, Journal.Community == 3 
                                 & PubYear == 1979 + i)
     pi.3[[i]] <- table(comm3.byyear[[i]]$AuthInstAffil) / dim(comm3.byyear[[i]])
     shannon.comm3[i] <- diversity(pi.3[[i]])
+    papers.comm3[i] <- dim(comm3.byyear[[i]])[1]
+
   }
   # author diversity model
   lead.shannon <- c(shannon.comm1, shannon.comm2, shannon.comm3)
   lead.year <- rep(1980:2014, times = 3)
   lead.community <- rep(1:3, each = 35)
+  lead.papers <- c(papers.comm1, papers.comm2, papers.comm3)
   
   require(mgcv)
   LeadDiv.fit <- lm(lead.shannon ~ lead.year * factor(lead.community))
@@ -35,6 +41,9 @@ AuthorDiversityPlots <- function(data.frame){
                        s(lead.year, by = factor(lead.community)) +
                        factor(lead.community))
   LeadDivCommonTrajectory.gam <- gam(lead.shannon ~ s(lead.year) + 
+                       factor(lead.community))
+  LeadDivPaperOffset.gam <- gam(lead.shannon ~ s(lead.papers) + s(lead.year) + 
+                       s(lead.year, by = factor(lead.community)) +
                        factor(lead.community))
   AIC(LeadDiv.fit)
   AIC(LeadDiv.gam)
@@ -46,6 +55,10 @@ AuthorDiversityPlots <- function(data.frame){
   comm1.leadgam <- gam(shannon.comm1 ~ s(lead.yr))
   comm2.leadgam <- gam(shannon.comm2 ~ s(lead.yr))
   comm3.leadgam <- gam(shannon.comm3 ~ s(lead.yr))
+  comm1.leadgam2 <- gam(shannon.comm1 ~ s(lead.yr) + papers.comm1)
+  comm2.leadgam2 <- gam(shannon.comm2 ~ s(lead.yr) + papers.comm2)
+  comm3.leadgam2 <- gam(shannon.comm3 ~ s(lead.yr) + papers.comm3)
+  
   leadpreds.comm1 <- predict(comm1.leadgam, type = "response", se.fit = TRUE)
   leadfit.comm1 <- leadpreds.comm1$fit 
   leadfit.up95.comm1 <-leadfit.comm1 - 1.96*leadpreds.comm1$se.fit 
@@ -74,7 +87,13 @@ AuthorDiversityPlots <- function(data.frame){
                                    ifelse(data.frame.no4$Journal.Community == 2, 1, 
                                           2))
   
-par(mfrow = c(1, 2), mar = c(2, 6, 1, 1), oma = c(1, 1, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
+  WithinGamFit <- gam(AuShannonDiv ~ s(PubYear) + 
+                        s(PubYear, by = factor(Journal.Community)) +
+                        factor(Journal.Community), data = data.frame.no4)
+  summary(WithinGamFit)
+
+
+par(mfrow = c(1, 1), mar = c(2, 6, 1, 1), oma = c(1, 1, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
 plot(data.frame.no4$AuShannonDiv ~ data.frame.no4$PubYear, 
        col = data.frame.no4$plotcol,
        pch = data.frame.no4$plotpch,
@@ -84,7 +103,7 @@ plot(data.frame.no4$AuShannonDiv ~ data.frame.no4$PubYear,
   lines(lowess(comm1$AuShannonDiv ~ comm1$PubYear), col = "gold", lwd = 3)
   lines(lowess(comm2$AuShannonDiv ~ comm2$PubYear), col = "red", lwd = 2)
   lines(lowess(comm3$AuShannonDiv ~ comm3$PubYear), col = "blue", lwd = 2)
-  mtext("(a)", side = 3, line = 0.5, at = 1990)
+#  mtext("(a)", side = 3, line = 0.5, at = 1990)
   leg.text <- c("Ecology", "Human epi", "Veterinary")
   legend("topleft", leg.text, col = c("gold", "red", "blue"),
          lty = c(1, 1, 1),
@@ -95,6 +114,18 @@ plot(data.frame.no4$AuShannonDiv ~ data.frame.no4$PubYear,
   axis(side = 1, at = c(1990, 1995, 2000, 2005, 2010, 2015), 
        labels = c("1990", "1995", "2000", "2005", "2010", "2015"), las = 1, cex.axis = 1)
   
+# Paul's check on number of authors vs. shannon diversity
+data.frame.sm <- subset(data.frame, NumberAuthors != 1 & NumberAuthors != 0 & NumberAuthors <= 20)
+par(mfrow = c(1, 2), oma = c(0, 0, 0, 0), mar = c(4, 4, 1, 1))
+plot(data.frame.sm$AuShannonDiv ~ data.frame.sm$NumberAuthors, 
+     xlab = "Number Authors",
+     ylab = "Within-paper Author Diversity")
+lines(lowess(data.frame.sm$AuShannonDiv ~ data.frame.sm$NumberAuthors))
+plot(lead.shannon ~ lead.papers, 
+     xlab = "Number Papers",
+     ylab = "Lead Author Diversity")
+lines(lowess(lead.shannon ~ lead.papers))
+
   # plot proportion of papers in each community with NO diversity thru time
 #   comm1.divtab <- as.vector(table(comm1[which(comm1$AuShannonDiv == 0),]$PubYear)) / 
 #     as.vector(table(comm1$PubYear))
@@ -105,41 +136,41 @@ plot(data.frame.no4$AuShannonDiv ~ data.frame.no4$PubYear,
 #   comm3.divtab <- as.vector(table(comm3[which(comm3$AuShannonDiv == 0),]$PubYear)) / 
 #     as.vector(table(comm3$PubYear))
   
-  comm1.divtab <- as.vector(table(comm1[which(comm1$AuShannonDiv == 0),]$BlockedYears)) / 
-    as.vector(table(comm1$BlockedYears))
-  
-  comm2.divtab <- as.vector(table(comm2[which(comm2$AuShannonDiv == 0),]$BlockedYears)) / 
-    as.vector(table(comm2$BlockedYears))
-  
-  comm3.divtab <- as.vector(table(comm3[which(comm3$AuShannonDiv == 0),]$BlockedYears)) / 
-    as.vector(table(comm3$BlockedYears))
-  
-  
-  plot((comm1.divtab ~ seq(1992, 2014, length.out = 12)), 
-       type = "l", ylab = "P(Author diversity = 0)", xaxt = "n", 
-       col = "gold", lwd = 2, ylim = c(0, 1), xlab = "", xlim = c(1990, 2015))
-  axis(side = 1, at = seq(1990, 2015, length.out = 6), 
-       labels = seq(1990, 2015, length.out = 6), 
-       las = 1, cex.axis = 1)
-#   lines(lowess(comm2.divtab ~ seq(1990:2014), f = 1/3), type = "l", col = "red",
+#   comm1.divtab <- as.vector(table(comm1[which(comm1$AuShannonDiv == 0),]$BlockedYears)) / 
+#     as.vector(table(comm1$BlockedYears))
+#   
+#   comm2.divtab <- as.vector(table(comm2[which(comm2$AuShannonDiv == 0),]$BlockedYears)) / 
+#     as.vector(table(comm2$BlockedYears))
+#   
+#   comm3.divtab <- as.vector(table(comm3[which(comm3$AuShannonDiv == 0),]$BlockedYears)) / 
+#     as.vector(table(comm3$BlockedYears))
+#   
+#   
+#   plot((comm1.divtab ~ seq(1992, 2014, length.out = 12)), 
+#        type = "l", ylab = "P(Author diversity = 0)", xaxt = "n", 
+#        col = "gold", lwd = 2, ylim = c(0, 1), xlab = "", xlim = c(1990, 2015))
+#   axis(side = 1, at = seq(1990, 2015, length.out = 6), 
+#        labels = seq(1990, 2015, length.out = 6), 
+#        las = 1, cex.axis = 1)
+# #   lines(lowess(comm2.divtab ~ seq(1990:2014), f = 1/3), type = "l", col = "red",
+# #         lwd = 2)
+# #   lines(lowess(c(NA, comm3.divtab) ~ seq(1990:2014), f = 1/3), type = "l", 
+# #         col = "blue", lwd = 2)
+#   lines((comm2.divtab ~ seq(1992, 2014, length.out = 12)), type = "l", col = "red",
 #         lwd = 2)
-#   lines(lowess(c(NA, comm3.divtab) ~ seq(1990:2014), f = 1/3), type = "l", 
+#   lines((c(comm3.divtab) ~ seq(1992, 2014, length.out = 12)), type = "l", 
 #         col = "blue", lwd = 2)
-  lines((comm2.divtab ~ seq(1992, 2014, length.out = 12)), type = "l", col = "red",
-        lwd = 2)
-  lines((c(comm3.divtab) ~ seq(1992, 2014, length.out = 12)), type = "l", 
-        col = "blue", lwd = 2)
-  points(comm1.divtab ~ seq(1992, 2014, length.out = 12), pch = 16, cex = 1, col = "gold")
-  points(comm2.divtab ~ seq(1992, 2014, length.out = 12), pch = 1, cex = 1, col = "red")
-  points(c(comm3.divtab) ~ seq(1992, 2014, length.out = 12), pch = 2, cex = 1, col = "blue")
-leg.text <- c("Ecology journals", "Human epi journals", "Veterinary journals")
-legend("bottomleft", leg.text, col = c("gold", "red", "blue"),
-         lty = c(1, 1, 1),
-         pch = c(16, 1, 2), 
-         cex = c(1, 1, 1),
-         lwd = c(2, 2, 2),
-         bty = "n")
-  mtext("(b)", side = 3, line = .5, at = 1990)  
+#   points(comm1.divtab ~ seq(1992, 2014, length.out = 12), pch = 16, cex = 1, col = "gold")
+#   points(comm2.divtab ~ seq(1992, 2014, length.out = 12), pch = 1, cex = 1, col = "red")
+#   points(c(comm3.divtab) ~ seq(1992, 2014, length.out = 12), pch = 2, cex = 1, col = "blue")
+# leg.text <- c("Ecology journals", "Human epi journals", "Veterinary journals")
+# legend("bottomleft", leg.text, col = c("gold", "red", "blue"),
+#          lty = c(1, 1, 1),
+#          pch = c(16, 1, 2), 
+#          cex = c(1, 1, 1),
+#          lwd = c(2, 2, 2),
+#          bty = "n")
+#   mtext("(b)", side = 3, line = .5, at = 1990)  
   
 #   # plot ratio of citations within to citations between by author diversity
 #   plot(shannon.comm1[- (1:10)] ~ seq(1990, 2014), 
