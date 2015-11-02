69217df305ff8bb7f117a6f61fbec1ab44ef3eec
Kezia Manlove <kezia.manlove@gmail.com>
2015-11-02 10:56:53 -0700

    Update GAMPlot function to build submitted Fig 4

--- R/GAMPlot.R
+++ R/GAMPlot.R
@@ -65,28 +65,8 @@ GAMPlot <- function(data.frame){
     
   }
   
-  # Shannon's diversity of lead author affiliation through time in each community
-  require(vegan)
-  comm1.byyear <- comm2.byyear <- comm3.byyear <- vector("list", 35)
-  pi.1 <- pi.2 <- pi.3 <- vector("list", 35)
-  shannon.comm1 <- shannon.comm2 <- shannon.comm3 <- rep(NA, 35)
   
-  for(i in 1:35){
-    comm1.byyear[[i]] <- subset(data.frame, Journal.Community == 1 
-                                & PubYear == 1979 + i)
-    pi.1[[i]] <- table(comm1.byyear[[i]]$AuthInstAffil) / dim(comm1.byyear[[i]])
-    shannon.comm1[i] <- diversity(pi.1[[i]])
-    
-    comm2.byyear[[i]] <- subset(data.frame, Journal.Community == 2 
-                                & PubYear == 1979 + i)
-    pi.2[[i]] <- table(comm2.byyear[[i]]$AuthInstAffil) / dim(comm2.byyear[[i]])
-    shannon.comm2[i] <- diversity(pi.2[[i]])
-    
-    comm3.byyear[[i]] <- subset(data.frame, Journal.Community == 3 
-                                & PubYear == 1979 + i)
-    pi.3[[i]] <- table(comm3.byyear[[i]]$AuthInstAffil) / dim(comm3.byyear[[i]])
-    shannon.comm3[i] <- diversity(pi.3[[i]])
-  }
+  # Shannon's diversity of lead author affiliation through time in each community
   
   data.frame$BlockedYears <- cut(data.frame$PubYear, breaks = seq(1990, 2014, length.out = 13))
   comm1 <- subset(data.frame, Journal.Community == 1)
@@ -96,45 +76,56 @@ GAMPlot <- function(data.frame){
   comm1.byyear <- comm2.byyear <- comm3.byyear <- vector("list", 35)
   pi.1 <- pi.2 <- pi.3 <- vector("list", 35)
   shannon.comm1 <- shannon.comm2 <- shannon.comm3 <- rep(NA, 35)
+  papers.comm1 <- papers.comm2 <- papers.comm3 <- rep(NA, 35)
   
   for(i in 1:35){
     comm1.byyear[[i]] <- subset(data.frame, Journal.Community == 1 
                                 & PubYear == 1979 + i)
     pi.1[[i]] <- table(comm1.byyear[[i]]$AuthInstAffil) / dim(comm1.byyear[[i]])
     shannon.comm1[i] <- diversity(pi.1[[i]])
+    papers.comm1[i] <- dim(comm1.byyear[[i]])[1]
     
     comm2.byyear[[i]] <- subset(data.frame, Journal.Community == 2 
                                 & PubYear == 1979 + i)
     pi.2[[i]] <- table(comm2.byyear[[i]]$AuthInstAffil) / dim(comm2.byyear[[i]])
     shannon.comm2[i] <- diversity(pi.2[[i]])
+    papers.comm2[i] <- dim(comm2.byyear[[i]])[1]
     
     comm3.byyear[[i]] <- subset(data.frame, Journal.Community == 3 
                                 & PubYear == 1979 + i)
     pi.3[[i]] <- table(comm3.byyear[[i]]$AuthInstAffil) / dim(comm3.byyear[[i]])
     shannon.comm3[i] <- diversity(pi.3[[i]])
+    papers.comm3[i] <- dim(comm3.byyear[[i]])[1]
   }
+  
   # author diversity model
   lead.shannon <- c(shannon.comm1, shannon.comm2, shannon.comm3)
   lead.year <- rep(1980:2014, times = 3)
   lead.community <- rep(1:3, each = 35)
+  lead.papers <- c(papers.comm1, papers.comm2, papers.comm3)
   
   require(mgcv)
   LeadDiv.fit <- lm(lead.shannon ~ lead.year * factor(lead.community))
   LeadDiv.gam <- gam(lead.shannon ~ s(lead.year) + 
                        s(lead.year, by = factor(lead.community)) +
                        factor(lead.community))
-  LeadDivCommonTrajectory.gam <- gam(lead.shannon ~ s(lead.year) + 
-                                       factor(lead.community))
+#  LeadDivCommonTrajectory.gam <- gam(lead.shannon ~ s(lead.year) + 
+#                                       factor(lead.community))
   AIC(LeadDiv.fit)
   AIC(LeadDiv.gam)
-  AIC(LeadDivCommonTrajectory.gam)
+#  AIC(LeadDivCommonTrajectory.gam)
   summary(LeadDiv.fit)
+  # BASIS FOR TABLE S6
   
   lead.yr <- 1980:2014
   
   comm1.leadgam <- gam(shannon.comm1 ~ s(lead.yr))
-  comm2.leadgam <- gam(shannon.comm2 ~ s(lead.yr))
-  comm3.leadgam <- gam(shannon.comm3 ~ s(lead.yr))
+   comm2.leadgam <- gam(shannon.comm2 ~ s(lead.yr))
+   comm3.leadgam <- gam(shannon.comm3 ~ s(lead.yr))
+#   comm1.leadgam.2 <- gam(shannon.comm1 ~ s(lead.yr) + s(papers.comm1))
+#   comm2.leadgam.2 <- gam(shannon.comm2 ~ s(lead.yr) + papers.comm2)
+#   comm3.leadgam.2 <- gam(shannon.comm3 ~ s(lead.yr) + papers.comm3)
+  
   leadpreds.comm1 <- predict(comm1.leadgam, type = "response", se.fit = TRUE)
   leadfit.comm1 <- leadpreds.comm1$fit 
   leadfit.up95.comm1 <-leadfit.comm1 - 1.96*leadpreds.comm1$se.fit 
@@ -149,6 +140,21 @@ GAMPlot <- function(data.frame){
   leadfit.comm3 <- leadpreds.comm3$fit
   leadfit.up95.comm3 <-leadfit.comm3 - 1.96*leadpreds.comm3$se.fit
   leadfit.low95.comm3 <-leadfit.comm3 + 1.96*leadpreds.comm3$se.fit
+# 
+#   leadpreds.comm1.2 <- predict(comm1.leadgam.2, type = "response", se.fit = TRUE)
+#   leadfit.comm1.2 <- leadpreds.comm1.2$fit 
+#   leadfit.up95.comm1.2 <-leadfit.comm1.2 - 1.96*leadpreds.comm1.2$se.fit 
+#   leadfit.low95.comm1.2 <-leadfit.comm1.2 + 1.96*leadpreds.comm1.2$se.fit
+#   
+#   leadpreds.comm2.2 <- predict(comm2.leadgam.2, type="response", se.fit = TRUE)
+#   leadfit.comm2.2 <- leadpreds.comm2.2$fit
+#   leadfit.up95.comm2.2 <- leadfit.comm2.2 - 1.96*leadpreds.comm2.2$se.fit
+#   leadfit.low95.comm2.2 <- leadfit.comm2.2 + 1.96*leadpreds.comm2.2$se.fit
+#   
+#   leadpreds.comm3.2 <- predict(comm3.leadgam.2, type="response", se.fit = TRUE)
+#   leadfit.comm3.2 <- leadpreds.comm3.2$fit
+#   leadfit.up95.comm3.2 <-leadfit.comm3.2 - 1.96*leadpreds.comm3.2$se.fit
+#   leadfit.low95.comm3.2 <-leadfit.comm3.2 + 1.96*leadpreds.comm3.2$se.fit
   
   
   require(mgcv)
@@ -181,6 +187,8 @@ GAMPlot <- function(data.frame){
                            factor(community), 
                          family = "binomial")
   summary(full.gam.fit)
+  # BASIS FOR TABLE S5
+
   AIC(full.gam.fit)
   AIC(reduced.gam.fit)
   
@@ -214,21 +222,79 @@ GAMPlot <- function(data.frame){
   fit.up95.comm3 <-fit.comm3 - 1.96*preds.comm3$se.fit
   fit.low95.comm3 <-fit.comm3 + 1.96*preds.comm3$se.fit
   
-  par(mfrow = c(1, 2), mar = c(2, 6, 1, 1), oma = c(1, 1, 1, 1))
-  plot(shannon.comm1[- (1:10)] ~ seq(1990, 2014), 
-       ylab = "Lead author affiliation diversity", xlab = "Year",
-       ylim = c(0, 3), pch = 16, col = "gold", type = "p", lwd = 2, 
-       xlim = c(1995, 2015))
-  polygon(c(lead.yr, rev(lead.yr)), 
-          c(leadfit.low95.comm1, rev(leadfit.up95.comm1)),
-          border=NA, col = rgb(255/255,215/255,0, alpha = .3))
-  polygon(c(lead.yr, rev(lead.yr)), 
-          c(leadfit.low95.comm2, rev(leadfit.up95.comm2)), col = rgb(1,0,0, alpha = .3),
-          border=NA)
-  polygon(c(lead.yr, rev(lead.yr)), 
-          c(leadfit.low95.comm3, rev(leadfit.up95.comm3)), col = rgb(0,0,1, alpha = .3),
-          border=NA)
-  
+  par(mfrow = c(1, 3), mar = c(2, 6, 1, 1), oma = c(1, 1, 1, 1))
+
+comm1.divtab <- as.vector(table(comm1[which(comm1$AuShannonDiv == 0),]$BlockedYears)) / 
+  as.vector(table(comm1$BlockedYears))
+
+comm2.divtab <- as.vector(table(comm2[which(comm2$AuShannonDiv == 0),]$BlockedYears)) / 
+  as.vector(table(comm2$BlockedYears))
+
+comm3.divtab <- as.vector(table(comm3[which(comm3$AuShannonDiv == 0),]$BlockedYears)) / 
+  as.vector(table(comm3$BlockedYears))
+# 
+# plot((comm1.divtab ~ seq(1992, 2014, length.out = 12)), 
+#      type = "l", ylab = "P(Author diversity = 0)", xaxt = "n", 
+#      col = "gold", lwd = 2, ylim = c(0, 1), xlab = "", xlim = c(1990, 2015))
+# axis(side = 1, at = seq(1990, 2015, length.out = 6), 
+#      labels = seq(1990, 2015, length.out = 6), 
+#      las = 1, cex.axis = 1)
+# #   lines(lowess(comm2.divtab ~ seq(1990:2014), f = 1/3), type = "l", col = "red",
+# #         lwd = 2)
+# #   lines(lowess(c(NA, comm3.divtab) ~ seq(1990:2014), f = 1/3), type = "l", 
+# #         col = "blue", lwd = 2)
+# lines((comm2.divtab ~ seq(1992, 2014, length.out = 12)), type = "l", col = "red",
+#       lwd = 2)
+# lines((c(comm3.divtab) ~ seq(1992, 2014, length.out = 12)), type = "l", 
+#       col = "blue", lwd = 2)
+# points(comm1.divtab ~ seq(1992, 2014, length.out = 12), pch = 16, cex = 1, col = "gold")
+# points(comm2.divtab ~ seq(1992, 2014, length.out = 12), pch = 1, cex = 1, col = "red")
+# points(c(comm3.divtab) ~ seq(1992, 2014, length.out = 12), pch = 2, cex = 1, col = "blue")
+# leg.text <- c("Ecology journals", "Human epi journals", "Veterinary journals")
+# legend("bottomleft", leg.text, col = c("gold", "red", "blue"),
+#        lty = c(1, 1, 1),
+#        pch = c(16, 1, 2), 
+#        cex = c(1, 1, 1),
+#        lwd = c(2, 2, 2),
+#        bty = "n")
+# mtext("(a)", side = 3, line = .5, at = 1990)  
+
+# plot(shannon.comm1[- (1:10)] ~ seq(1990, 2014), 
+#        ylab = "Lead author affiliation diversity", xlab = "Year",
+#        ylim = c(-1, 3), pch = 16, col = "gold", type = "p", lwd = 2, 
+#        xlim = c(1995, 2015))
+#   lines(leadfit.low95.comm1.2 ~ lead.yr)
+#   lines(leadfit.up95.comm1.2 ~ lead.yr)
+# polygon(c(lead.yr, rev(lead.yr)), 
+#           c(leadfit.low95.comm1.2, rev(leadfit.up95.comm1.2)),
+# #          c(leadfit.low95.comm1.2, rev(leadfit.up95.comm1.2)),
+#           border=NA, col = rgb(255/255,215/255,0, alpha = .3))
+#   polygon(c(lead.yr, rev(lead.yr)), 
+#           c(leadfit.low95.comm2.2, rev(leadfit.up95.comm2.2)), col = rgb(1,0,0, alpha = .3),
+#           border=NA)
+#   polygon(c(lead.yr, rev(lead.yr)), 
+#           c(leadfit.low95.comm3.2, rev(leadfit.up95.comm3.2)), col = rgb(0,0,1, alpha = .3),
+#           border=NA)
+ 
+#svg("./Figures/Figure4_PNASSpec_20151017.svg", width = 8, heigh = 4)
+par(mfrow = c(1, 2), oma = c(0, 0, 0, 1), mar = c(2, 4, 2, 0))
+plot(shannon.comm1[- (1:10)] ~ seq(1990, 2014), 
+     ylab = "Lead author affiliation diversity", xlab = "Year",
+     ylim = c(-1, 3), pch = 16, col = "gold", type = "p", lwd = 2, 
+     xlim = c(1995, 2015))
+#lines(leadfit.low95.comm1 ~ lead.yr)
+#lines(leadfit.up95.comm1 ~ lead.yr)
+polygon(c(lead.yr, rev(lead.yr)), 
+        c(leadfit.low95.comm1, rev(leadfit.up95.comm1)),
+        #          c(leadfit.low95.comm1.2, rev(leadfit.up95.comm1.2)),
+        col = rgb(255/255,215/255,0, alpha = .3), border="gold")
+polygon(c(lead.yr, rev(lead.yr)), 
+        c(leadfit.low95.comm2, rev(leadfit.up95.comm2)), col = rgb(1,0,0, alpha = .3),
+        border="red")
+polygon(c(lead.yr, rev(lead.yr)), 
+        c(leadfit.low95.comm3, rev(leadfit.up95.comm3)), col = rgb(0,0,1, alpha = .3),
+        border="blue")
+
   points(shannon.comm1[-c(1:10)] ~ seq(1990, 2014), col = "gold", pch = 16, lwd = 2)
   #  lines(shannon.comm2[-c(1:10)] ~ seq(1990, 2014), col = "red", lwd = 2)
   #  lines(shannon.comm3[-c(1:10)] ~ seq(1990, 2014), col = "blue", lwd = 2)
@@ -260,13 +326,13 @@ plot(x = 0, y = 0, xlab = "",
 #      xaxs = "i", yaxs = "i")
 polygon(c(years, rev(years)), 
         c(fit.low95.comm1, rev(fit.up95.comm1)),
-        border=NA, col = rgb(255/255,215/255,0, alpha = .3))
+        border="gold", col = rgb(255/255,215/255,0, alpha = .3))
 polygon(c(years[-1], rev(years[-1])), 
         c(fit.low95.comm2, rev(fit.up95.comm2)), col = rgb(1,0,0, alpha = .3),
-        border=NA)
+        border="red")
 polygon(c(years[-c(1:3)], rev(years[-c(1:3)])), 
         c(fit.low95.comm3, rev(fit.up95.comm3)), col = rgb(0,0,1, alpha = .3),
-        border=NA)
+        border="blue")
 points(comm1.quants.byyear[-c(1:10)] ~ seq(1990, 2014), col = "gold", pch = 1)
 points(comm2.quants.byyear[-c(1:15)] ~ seq(1995, 2014), col = "red", pch = 1)
 points(comm3.quants.byyear[-c(1:10)] ~ seq(1990, 2014), col = "blue", pch = 2)
@@ -279,5 +345,6 @@ legend("topright", leg.text, col = c("gold", "red", "blue"), bty = "n",
        lty = c(1, 1, 1),
        lwd = c(2, 2, 2), cex = .8)
 mtext("(b)", line = 0.5, at = 1995)
+#dev.off()
 
 }
